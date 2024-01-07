2217f611f96a90439064386a1576e6b12166c9cd
soph78 <144708941+soph78@users.noreply.github.com>
2024-01-07 13:39:43 -0500

    Background color

---
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas (windowWidth, windowHeight); //this creates the window W/H to be the size of my browser windowWidth
+  background (139, 151, 173); // grey/blue color background
+}
+
+function draw() {
+}
