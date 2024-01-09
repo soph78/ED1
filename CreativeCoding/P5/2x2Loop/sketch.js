0a8c90ab4b1e866a6f1c9b68e102cc2c05be3b48
soph78 <144708941+soph78@users.noreply.github.com>
2024-01-09 11:21:21 -0500

    2x2Loop

---
+++ CreativeCoding/P5/2x2Loop/sketch.js
@@ -0,0 +1,24 @@
+function setup() { // runs once 
+  createCanvas(720,720);
+  background (255,0,0);
+  fill ("teal");
+  strokeWeight(8);
+}
+
+function draw() { 
+  background(220);
+  
+  translate(60,60);
+ 
+  for (var y = 0; y < 600; y = y + 300){
+    for (var x = 0; x < 600; x = x + 300) {
+    quad (x, y,
+       x + 300, y, 
+       x + 300, y+300,
+       x, y+300); 
+    } 
+  }
+  
+  
+  noLoop(); 
+}
