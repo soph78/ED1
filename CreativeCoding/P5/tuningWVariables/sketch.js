fa8ccbc8c8a8d4d1641e0c3247b7e8ee1185d310
soph78 <144708941+soph78@users.noreply.github.com>
2024-01-09 11:44:32 -0500

    tuningWVariables

--- CreativeCoding/P5/tuningWVariables/sketch.js
+++ CreativeCoding/P5/tuningWVariables/sketch.js
@@ -1,5 +1,24 @@
-function setup() {
+function setup() { // runs once 
+  createCanvas(windowWidth, windowHeight);
+  fill ("teal");
+  strokeWeight(1);
 }
 
-function draw() {
+function draw() { 
+  
+  var num = 24; 
+  var sideLen = windowWidth/num;
+ 
+  for (var y = 0; y < windowHeight; y = y + sideLen){
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+    quad (x, y,
+       x + sideLen, y, 
+       x + sideLen, y+sideLen,
+       x, y+sideLen); 
+    } 
+  }
+}
+
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight); 
 }
