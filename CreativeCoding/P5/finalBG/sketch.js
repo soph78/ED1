d4866283ed7ea877cae1a488a1eb8ea36b0249a5
soph78 <144708941+soph78@users.noreply.github.com>
2024-01-10 13:46:29 -0500

    add gif to grid

---
+++ CreativeCoding/P5/finalBG/sketch.js
@@ -0,0 +1,30 @@
+// adding a GIF to my grid 
+
+let gif; 
+
+function preload ('assets/LinGIF.gif');
+
+
+function setup() { // runs once 
+  createCanvas(windowWidth, windowHeight);
+  fill ("teal");
+  strokeWeight(1);
+}
+
+function draw() { 
+
+  background('teal');
+  
+  var num = 24; 
+  var sideLen = windowWidth/num;
+ 
+  for (var y = 0; y < windowHeight; y = y + sideLen){
+    for (var x = 0; x < windowWidth; x = x + sideLen) {
+    	image(gif, x, y, sideLen, sideLen); 
+    } 
+  }
+}
+
+function windowResized(){
+  resizeCanvas (windowWidth, windowHeight); 
+}
