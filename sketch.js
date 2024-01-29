9b8000e61e9aa9ad565d4564cae8369785191f21
soph78 <144708941+soph78@users.noreply.github.com>
2024-01-29 09:28:51 -0500

    code fix

--- sketch.js
+++ sketch.js
@@ -20,8 +20,8 @@ function draw() {
   var num = 24; 
   var sideLen = windowWidth/num;
  
-  for (var y = 0; y < windowHeight; y = y + sideLen){
-    for (var x = 0; x < windowWidth; x = x + sideLen) {
+  for (var y = 0; y < 2*windowHeight; y = y + sideLen){
+    for (var x = 0; x < 2*windowWidth; x = x + sideLen) {
     	image(gif, x, y, sideLen, sideLen); 
     } 
   }
